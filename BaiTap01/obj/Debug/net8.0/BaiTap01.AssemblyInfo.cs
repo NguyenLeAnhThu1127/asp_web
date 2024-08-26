538d63fece8fa318dc7ddfdfb87a32af7d05a073
NguyenLeAnhThu1127 <1822040540@dntu.edu.vn>
2024-08-26 08:20:01 +0700

    Config auto build

--- BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
+++ BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+846f5118225db1fc72e69f7b76dea611f25e5f8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f96ab91620395d59bf1da3fe300a6b1ed3e665")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
