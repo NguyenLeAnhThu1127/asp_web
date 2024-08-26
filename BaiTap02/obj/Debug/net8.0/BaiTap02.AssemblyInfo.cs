f10d735a2e098eb500f2d78738068f12bbffba22
NguyenLeAnhThu1127 <1822040540@dntu.edu.vn>
2024-08-26 15:07:24 +0700

    Init project BaiTap02

--- BaiTap02/obj/Debug/net8.0/BaiTap02.AssemblyInfo.cs
+++ BaiTap02/obj/Debug/net8.0/BaiTap02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538d63fece8fa318dc7ddfdfb87a32af7d05a073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e26537f2ab8c8e5a805884b642e3cc4e76ce89")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
