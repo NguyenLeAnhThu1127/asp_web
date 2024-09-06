bdd3d988b0ed2c903154ee3ca12f8d028f82c026
NguyenLeAnhThu1127 <1822040540@dntu.edu.vn>
2024-09-06 15:10:04 +0700

    Init Project BaiTap05

--- BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
+++ BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f96ab91620395d59bf1da3fe300a6b1ed3e665")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538d63fece8fa318dc7ddfdfb87a32af7d05a073")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BaiTap05/Models/TheLoaiViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Models
+{
+    public class TheLoaiViewModel 
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
